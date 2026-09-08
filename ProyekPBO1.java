3351d84ee3d20c33a88089bc05115cbe21cd8456
Faiz Naufal <sorgama1007@gmail.com>
2026-09-08 19:26:15 +0700

    Praktikum2

---
+++ ProyekPBO1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyekpbo1;
+
+/**
+ *
+ * @author ASV
+ */
+public class ProyekPBO1 {
+
+    public static void main(String[] args) {
+        String nama, alamat, no_hp;
+        long npm;
+        
+        nama = "Faiz Naufal Hersorgama";
+        npm = 2505060040L;
+        alamat = "Magelang";
+        no_hp = "085729005777";
+        
+        System.out.println("Nama:" + nama);
+        System.out.println("NPM:" + npm);
+        System.out.println("Alamat:" + alamat);
+        System.out.println("No_hp:" + no_hp);
+        
+    }
+}
